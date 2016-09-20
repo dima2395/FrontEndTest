8f6a1a185141bc702d76b14c40f6fb4e5eda3ceb
Gvozdinsky Dmitry <gdima2395@gmail.com>
2016-09-20 14:04:46 +0300

    added functionality to delete posts from localStorage and html

--- js/common.js
+++ js/common.js
@@ -11,16 +11,27 @@
           result = template(wrapper),
           container = document.getElementById(id);
       container.innerHTML = result;
-
     }
 
-    $.getJSON('../json/posts.json', function(posts) { // get json object 
-
+    $.getJSON('../json/posts.json', function(posts) {
       localStorage.setItem('posts', JSON.stringify(posts)); // add posts to localStorage
+    });
 
-    }); // end getJSON
+    printPosts('posts'); 
 
-    printPosts('posts');
+    $('#posts').on('click', '.btn-delete', function(e) { // delete elements from localStorage and from html on event click
+      var $this = $(this),
+          postIdToDelete = $this.parent().parent().data('postId'), // get post id from article data attribute(data-post-id)
+          posts = JSON.parse(localStorage.getItem('posts'));
+
+      for(var i = 0; i < posts.length; i++) {
+        if(postIdToDelete == posts[i]['id']) {
+          posts.splice(i, 1);
+          localStorage.setItem('posts', JSON.stringify(posts));
+          $this.parent().parent().remove();
+        }
+      }
+    });
 
   });
 
